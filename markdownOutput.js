8b7a31c2222abc51cb975a7e3a16bd2cfaa5ad26
Frederico Swarthings <timscallon1@gmail.com>
2024-07-25 16:37:56 -0500

    readme added!

--- markdownOutput.js
+++ markdownOutput.js
@@ -25,9 +25,6 @@ function markdownOutput(mdObj) {
 
   ## Tests
   ${mdObj.tests}
-
-  ## License
-  ${mdObj.licenseBadge ? `${mdObj.licenseBadge}\n` : ''} This project is using the ${mdObj.license}. Please click the badge icon for more information, or refer directly to the LICENSE in the repo.
   
   ## Credits
   ${mdObj.credits}
@@ -35,6 +32,9 @@ function markdownOutput(mdObj) {
   ## Questions
   - [Check me out on Github!](https://www.github.com/${mdObj.github})
   - [Email Me](mailto:${mdObj.email}?subject=Hello!)
+
+  ## License
+  ${mdObj.licenseBadge ? `${mdObj.licenseBadge}\n` : ''} This project is using the ${mdObj.license}. Please click the badge icon for more information, or refer directly to the LICENSE in the repo.
 `
 return markdown;
 }
